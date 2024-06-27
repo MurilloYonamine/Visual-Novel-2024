95b7df25803947ea6a1fda9762391b2b0b0f7b6b
MurilloYonamine <murillo.gyonamine@gmail.com>
2024-06-27 19:57:58 -0300

    I don't remember what i have done, but i'm commiting it

---
+++ Assets/Scripts/Core/DialogueContainer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+// The dialogue graphical display with all dialogue box elements. Can be altered for diferrent looks
+[Serializable]
+public class DialogueContainer
+{
+    public GameObject root;
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+}
---
+++ Assets/Scripts/Core/DialogueSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The main controller for stating and controlling dialogue and conversations on screen.
+public class DialogueSystem : MonoBehaviour
+{
+    [SerializeField] private DialogueContainer dialogueContainer = new DialogueContainer();
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
